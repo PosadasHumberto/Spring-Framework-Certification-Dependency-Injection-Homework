07048a6aef407c3de0d3ca5dd713ba518321e1f1
Humberto Posadas <posadas.humberto@outlook.com>
2023-03-28 09:42:53 +0200

    setting up project structure

---
+++ src/main/java/org/hposadas/dependencyinjectionhomework/DependencyInjectionHomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.hposadas.dependencyinjectionhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hposadas/dependencyinjectionhomework/DependencyInjectionHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hposadas.dependencyinjectionhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
